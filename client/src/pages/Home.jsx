2db45427522cc444728a518b48c2a3256d1281af
unmesh-varade <158160261+uv-19@users.noreply.github.com>
2025-07-29 20:37:10 +0530

    Added Authentication using Clerk and Hero page frontend.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import AiTools from '../components/AiTools'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-    </div>
+    <>
+        <Navbar />
+        <Hero/>
+        <AiTools/>
+        <Testimonial/>
+    </>
   )
 }
 
